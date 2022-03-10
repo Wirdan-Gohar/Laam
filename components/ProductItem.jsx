bbe4fc05d4551a976f4f0565f060ff0335850dc4
Raja Areeb <areebraja000@gmail.come>
2022-03-10 23:09:07 +0500

    thursday

---
+++ components/ProductItem.jsx
@@ -0,0 +1,150 @@
+import {
+    FavoriteBorderOutlined,
+    SearchOutlined,
+    ShoppingCartOutlined,
+  } from "@material-ui/icons";
+  import styled from "styled-components";
+  import Link from "next/link";
+  import { useEffect, useState } from 'react';
+  
+  
+  
+  
+  const ProductItem = ({ item }) => {
+    const [path,setPath]=useState('')
+   
+  
+    // const [items, setItems] = useState([])
+    useEffect(() => {
+      
+      
+     
+    }, [])
+    return (
+  
+  
+      <Container>
+        
+        {/* <Circle /> */}
+        {/* <div style={{display:'flex',flexDirection:'column'}}> */}
+        <Image src={item.img}/>
+        {/* <PriceCol>
+            {item.name}
+        </PriceCol> */}
+       {/* </div> */}
+
+        <Info>
+          <Icon>
+            <ShoppingCartOutlined />
+          </Icon>
+          <Link href='/product/[id]' as={`/product/${item.id}`} >
+            <Icon >
+              <SearchOutlined />
+            </Icon>
+          </Link>
+          <Icon>
+            <FavoriteBorderOutlined />
+          </Icon>
+  
+        </Info>
+        
+      </Container>
+    );
+  };
+  
+  export default ProductItem;
+  
+  
+  
+  
+  const Info = styled.div`
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+    z-index: 3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.5s ease;
+    cursor: pointer;
+  `;
+  
+  const Container = styled.div`
+    flex: 1;
+    margin: 5px;
+    min-width: 280px;
+    height: 350px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+   
+    position: relative;
+    &:hover ${Info}{
+      opacity: 1;
+    }
+  `;
+  
+  const Circle = styled.div`
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: white;
+    position: absolute;
+  `;
+
+const PriceCol = styled.div`
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: red;
+    position: absolute;
+  `;
+
+  
+  const Image = styled.img`
+    height: 100%;
+    z-index: 2;
+  `;
+  
+  const Icon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+    transition: all 0.5s ease;
+    &:hover {
+      background-color: #e9f5f5;
+      transform: scale(1.1);
+    }
+  `;
+  const Title = styled.p`
+  font-size: 14px;
+  /* display: flex; */
+  margin-top: 300px;
+  margin-left: 300px;
+  align-items: center;
+  justify-content: space-between;
+  
+  `;
+  const TInfo = styled.div`
+  position: absolute;
+  margin-right: 100%;
+ 
+  `;
+  
+  const StyledLink = styled(Link)`
+                  color: #000000;
+                  &:focus, &:hover, &:visited, &:link, &:active {
+                      text-decoration: none;
+      }
+                  `;
+  
--- pages/category/[id]/index.jsx
+++ pages/category/[id]/index.jsx
@@ -3,8 +3,11 @@ import styled from "styled-components";
 import Navbar from "../../../components/Navbar"
 import Footer from "../../../components/footer/Footer";
 import { useEffect, useState } from 'react';
+import {
+    HomeOutlined
+  } from "@material-ui/icons";
 //import axios from "axios";
-//import ProductItem from "../../../components/ProductItem";
+import ProductItem from "../../../components/ProductItem";
 
 import { useRouter } from 'next/router'
 
@@ -12,7 +15,62 @@ const Category = () => {
     const router = useRouter();
     const { id } = router.query;
     const [items, setItems] = useState([])
-    const [spro, setSizeProduct] = useState([])
+    const [pro, setPro] = useState([
+        {
+            id:1,
+            img:'//cdn.shopify.com/s/files/1/2337/7003/products/2_ae09fef5-fd82-4c9b-aa0f-12383c54d218_300x.jpg?v=1646656226',
+            name:'ABA',
+            price:4000
+        },
+        {
+            id:2,
+            name:'ABA',
+            img:'//cdn.shopify.com/s/files/1/2337/7003/products/2_ae09fef5-fd82-4c9b-aa0f-12383c54d218_300x.jpg?v=1646656226',
+            price:6000
+        },
+        {
+            id:3,
+            name:'ABA',
+            img:'//cdn.shopify.com/s/files/1/2337/7003/products/2_ae09fef5-fd82-4c9b-aa0f-12383c54d218_300x.jpg?v=1646656226',
+            price:7000
+        },
+        {
+            id:4,
+            name:'ABA',
+            img:'//cdn.shopify.com/s/files/1/2337/7003/products/2_ae09fef5-fd82-4c9b-aa0f-12383c54d218_300x.jpg?v=1646656226',
+            price:4000
+        },
+        {
+            id:5,
+            name:'ABA',
+            img:'//cdn.shopify.com/s/files/1/2337/7003/products/2_ae09fef5-fd82-4c9b-aa0f-12383c54d218_300x.jpg?v=1646656226',
+            price:9000
+        },
+        {
+            id:6,
+            name:'ABA',
+            img:'//cdn.shopify.com/s/files/1/2337/7003/products/2_ae09fef5-fd82-4c9b-aa0f-12383c54d218_300x.jpg?v=1646656226',
+            price:7000
+        },
+        {
+            id:8,
+            name:'ABA',
+            img:'//cdn.shopify.com/s/files/1/2337/7003/products/2_ae09fef5-fd82-4c9b-aa0f-12383c54d218_300x.jpg?v=1646656226',
+            price:3000
+        },
+        {
+            id:9,
+            name:'ABA',
+            img:'//cdn.shopify.com/s/files/1/2337/7003/products/2_ae09fef5-fd82-4c9b-aa0f-12383c54d218_300x.jpg?v=1646656226',
+            price:4000
+        },
+        {
+            id:10,
+            name:'ABA',
+            img:'//cdn.shopify.com/s/files/1/2337/7003/products/2_ae09fef5-fd82-4c9b-aa0f-12383c54d218_300x.jpg?v=1646656226',
+            price:9000
+        }
+    ])
     const [cat, setCategory] = useState({
         id:null,
         name:'',
@@ -52,13 +110,20 @@ const Category = () => {
         <Container>
            
             <Navbar />
-
-            <Title>Home</Title>
+{/* 
+            <Title>Home</Title> */}
             
             <FilterContainer>
             
-            
-                <Filter>
+            <FilterHome>
+                <FilterHomeText>
+                    <HomeOutlined/>
+                    </FilterHomeText>
+                    <FilterHomeText>
+                        {">"} Unstiched {">"} Lawn
+                    </FilterHomeText>
+            </FilterHome>
+                {/* <Filter>
                     <FilterText>Filter Products:</FilterText>
                     <Select onChange={handleSelectChange}>
                         <Option disabled selected>
@@ -81,24 +146,60 @@ const Category = () => {
                         <Option>large</Option>
                         <Option>xl</Option>
                     </Select>
-                </Filter>
-                <Filter>
-                    <FilterText>Sort Products:</FilterText>
-                    <Select>
-                        <Option selected>Newest</Option>
+                </Filter>*/}
+                
+                    <SortText></SortText>
+                        <Select>
+                        <Option selected>Best Selling</Option>
                         <Option>Price (asc)</Option>
                         <Option>Price (desc)</Option>
                     </Select>
-                </Filter>
+               
                
 
             </FilterContainer>
+
+            <GridArea>
+            <LeftBar>
+            <Filter>  
+            <FilterTitle>FILTER PRODUCTS:</FilterTitle>
+            </Filter> 
+            <FilterTitle>Price</FilterTitle> 
+            <Filter>
+              <FilterText>
+                  <input type='radio' id='price'/><label for="price">2000-3000</label></FilterText>  
+            </Filter>
+            <FilterTitle>Delivery</FilterTitle> 
+            <Filter>
+              <FilterText> <input  type='radio' id='w1'/>
+              <label for="w1">1-2 Weeks </label>
+              </FilterText>  
+              <FilterText>
+                  <input type='radio' id='w2'/><label for="w2"> 2-4 Weeks</label>
+                  </FilterText>  
+            </Filter>
+             
+            <FilterTitle>Category</FilterTitle> 
+            <Filter>
+              <FilterText>
+                  <input type='radio' id='price'/><label for="price">Unstiched</label></FilterText>  
+            </Filter>
+
+            </LeftBar>   
             <Productshow>
-                
+              {pro.map(it=>(
+                 <ProductItem item={it}/>
+              ))
+
+              }         
        
             </Productshow>
 
-            <Footer />
+
+            </GridArea>
+            
+
+           
         </Container>
        
     );
@@ -119,18 +220,66 @@ const Title = styled.h1`
 `;
 
 const FilterContainer = styled.div`
+margin-top:80px;
   display: flex;
+  justify-content: space-between;
+`;
+const GridArea = styled.div`
+  display: flex;
+  flex-direction:row;
+  height:200px;
+  
   justify-content: space-between;
 `;
 
 const Filter = styled.div`
-  margin: 20px;
+  //margin: 20px;
+  display:flex;
+  flex-direction:column;
+  
+  //height:40px;
+  background-color:white;
+`;
+const FilterHome = styled.div`
+  //margin: 20px;
+  display:flex;
+  flex-direction:row;
+  margin-left:30px
+  //height:40px;
+  
+`;
+const LeftBar = styled.div`
+  margin-left:30px;
+  margin-top:40px;
+  background-color:whitesmoke;
+  width:600px;
+  height:500px;
+  border-style:groove;
 `;
 
 const FilterText = styled.span`
-  font-size: 20px;
-  font-weight: 600;
-  margin-right: 20px;
+  font-size: 14px;
+  padding:20px;
+  margin-top: 10px;
+  margin-left:20px
+`;
+const FilterHomeText = styled.span`
+  font-size: 14px;
+  padding:6px;
+  /* padding:20px;
+  margin-top: 10px;
+  margin-left:20px */
+`;
+const SortText = styled.span`
+  font-size: 14px;
+  
+  
+`;
+const FilterTitle = styled.span`
+  font-size: 16px;
+  font-weight: 700;
+  padding:20px;
+  margin: 20px;
 `;
 
 const Select = styled.select`
@@ -142,6 +291,7 @@ const Option = styled.option``;
 const Productshow = styled.div`
     padding: 20px;
     display: flex;
+    flex-direction:row;
     flex-wrap: wrap;
     justify-content: space-between;
 `;
